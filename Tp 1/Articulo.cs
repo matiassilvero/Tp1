cd4a6ae7bea81ecfb205d34328e523259c8f7ef7
Enzo Matias <matiassilvero3@gmail.com>
2023-04-29 17:21:45 -0300

    Base de Tp y prueba de subida con GH

---
+++ Tp 1/Articulo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tp_1
+{
+    internal class Articulo 
+    {
+        string idArticulo;
+        string nombre;
+        string descripcionArticulo;
+        int idMarca;
+        int idCategoria;
+        int idImagen;
+        float precio;
+
+    }
+}
